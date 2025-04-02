25e0c240b80699b9088ba533b8985b63b6a97540
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 16:07:41 -0500

    Add files via upload

---
+++ Week 6/AdvancdBreakpointHomeworkApp/ConsoleUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                try
+                {
+                    if (i == 73)
+                    {
+                        throw new Exception("i is 73");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine($"An error has occurred: {ex.Message}");
+                }
+                Console.WriteLine($"The value of i is {i}");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
