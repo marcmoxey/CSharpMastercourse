c56eb03798088964eb4990a1b1e0b71c7f0a53da
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 15:59:43 -0500

    Add files via upload

---
+++ Week 3/SwitchDemoApp/SwitchDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿
+string firstName = "Marc";
+int age = 23;
+
+
+switch (age)
+{
+	case >= 0 and < 18:
+        Console.WriteLine("You are a child");
+		break;
+	case >= 18 and < 66:
+        Console.WriteLine("You should have a job");
+		break;
+	case >= 66:
+		Console.WriteLine("Hopefully you are retired or retiring soon.");
+		break;
+	default:
+        Console.WriteLine("Age was not in expect range");
+		break;
+}
+
+
+// not evaluating to true or false
+//switch (firstName.ToLower())
+//{
+//    // compare to value on the switch
+//    //case "sue":
+//    case "marc" or "sue":
+//        Console.WriteLine("Hello Professor");
+//        break;
+//    case "tim":
+//        Console.WriteLine("Hello tim");
+//        break;
+//    default:
+//        Console.WriteLine("I don't know you");
+//        break;
+
+//}       
