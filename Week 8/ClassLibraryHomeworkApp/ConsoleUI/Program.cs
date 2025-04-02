a646bace564c1ca1ac125361d4a7ff58566a3b78
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 17:26:12 -0500

    Add files via upload

---
+++ Week 8/ClassLibraryHomeworkApp/ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonLibrary.Models;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            PersonModel person = new PersonModel();
+            string firstName = person.FirstName = "Marc";
+            string lastName = person.LastName = "Moxey";
+            Console.WriteLine($"{firstName} {lastName}");
+            Console.ReadLine(); 
+        }
+    }
+}
