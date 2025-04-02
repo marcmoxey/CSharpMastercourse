47164dd8192fd5118799d1aa42aae043f3f1cf9b
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 15:57:46 -0500

    Add files via upload

---
+++ Week 3/DecimalVariablesApp/DecimalVariables/Program.cs
@@ -0,0 +1,5 @@
+﻿// 4.12, 32, 1.234
+
+decimal bankAccountBalance; // more precise, doesn't round till after 28 position, used for money
+bankAccountBalance = 2.34M;
+Console.WriteLine(bankAccountBalance);
---
+++ Week 3/DecimalVariablesParcticeProjectApp/DecimalVariablesParcticeProject/Program.cs
@@ -0,0 +1,10 @@
+﻿
+decimal bankAccountBalance = 1674.38M;
+decimal carPayment = 487M;
+decimal carInsurance = 512.94M;
+decimal appleMusic = 10.99M;
+
+decimal leftOverAmount = bankAccountBalance -  carPayment;
+Console.WriteLine(leftOverAmount);
+leftOverAmount = leftOverAmount - carInsurance;
+Console.WriteLine(leftOverAmount);
