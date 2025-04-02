781b2ebca347965cd0abea91189e11f545af12f6
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 16:01:05 -0500

    Add files via upload

---
+++ Week 3/TypeConversionHomeworkApp/TypeConversionHomework/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * 
+ * Capture a user's age from the Console and 
+ * then identify how old they will be in 25 years, as well
+ * as how old they were 25 years ago. Print that
+ * information to the Console in natural language
+ */
+
+
+Console.Write("How old are you: ");
+string ageText = Console.ReadLine();    
+int.TryParse(ageText, out int age);
+int ageIn25Years = age + 25;
+int age25YearsAgo = age - 25;
+
+Console.WriteLine($"You will be {ageIn25Years} year old in 25 years");
+Console.WriteLine($"You were {age25YearsAgo} year old 25 years ago");
---
+++ Week 3/TypeConversionParcticeProjectApp/TypeConversionParcticeProject/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.Write("How much money do you have in your bank account: ");
+string bankBalanceText = Console.ReadLine();
+
+bool isValidDecimal = decimal.TryParse(bankBalanceText, out decimal bankBalance);
+Console.WriteLine($"The decimal is {isValidDecimal}. The amount in the bank account is {bankBalance}");
+Console.ReadLine();
