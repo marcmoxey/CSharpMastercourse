3e85e0f33b544eb9537f7b573f5e8172d225df45
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 16:00:26 -0500

    Add files via upload

---
+++ Week 3/SwtichParcticeProjectApp/SwtichParcticeProject/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Console.Write("What is your age: ");
+string ageText  = Console.ReadLine();
+int.TryParse(ageText, out int age); 
+
+switch(age)
+{
+    case < 16:
+        Console.WriteLine("You can't get your learners permit yet");
+        break;
+    case >= 16 and < 18:
+        Console.WriteLine("You can get your license");
+        break;
+    case <= 20:
+        Console.WriteLine("You can't drink yet");
+        break;
+    default:
+        Console.WriteLine("You can drink");
+        break;  
+}
