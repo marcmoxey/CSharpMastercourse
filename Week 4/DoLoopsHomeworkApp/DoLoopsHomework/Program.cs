dcb7193f88c9237126a536073abc40a93b62c12f
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 16:03:44 -0500

    Add files via upload

---
+++ Week 4/DoLoopsHomeworkApp/DoLoopsHomework/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string userInput;
+
+do 
+{
+    Console.Write("What is your name(or type 'exit' to quit application): ");
+    userInput = Console.ReadLine();
+    if (userInput.ToLower() == "tim" )
+    {
+        Console.WriteLine($"Welcome professor {userInput}");
+    } 
+    else
+    {
+        Console.WriteLine($"Welcome {userInput}");
+    }
+} while (userInput.ToLower() != "exit");
