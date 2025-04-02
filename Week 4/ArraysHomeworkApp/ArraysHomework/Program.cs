b66f335adbc6fb83e12a77d9cbd6ece5846a35c4
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 16:02:39 -0500

    Add files via upload

---
+++ Week 4/ArraysHomeworkApp/ArraysHomework/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string[] names = new string[] {"Marc","Jon","Nio" };
+
+Console.Write("Please enter a number: ");
+string userInput = Console.ReadLine();
+int.TryParse(userInput, out int userNum);
+
+if(userNum > names.Length - 1)
+{
+    Console.WriteLine($"Please enter a number in range 0 - {names.Length - 1}");
+} else
+{
+    Console.WriteLine(names[userNum]);
+}
---
+++ Week 4/DicitionariesApp/Dicitionaries/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Dictionary<string, string> lookup = new Dictionary<string, string>();
+lookup["animal"] = "Not a human";
+lookup["fish"] = "Not a human that swim";
+lookup["Human"] = "Us";
+
+
+Console.WriteLine($"The definition of fish is {lookup["fish"]}");
+
+Dictionary<int, string> employee = new Dictionary<int, string>();
+employee[95] = "Marc Moxey";
+employee[28] = "Sue Storm";
+employee[22] = "Tim Corey";
+
+//Console.WriteLine($"The employee with the id 28 is {employee[29]}"); // need to be a valid key
+
+Dictionary<string, int> dayOfWeek = new Dictionary<string, int>();
+dayOfWeek["Wednesday"] = 4;
+dayOfWeek["Thursday"] = 5;
+dayOfWeek["Friday"] = 6;
+//dayOfWeek["Friday"] = 7; // can't have dupe keys
+
+Console.WriteLine(dayOfWeek.Remove("Thursday"));
+foreach(var day in dayOfWeek)
+{
+    Console.WriteLine(day);
+}
+//Console.WriteLine($"Wednesday is day number {dayOfWeek["Wednesday"]}");
