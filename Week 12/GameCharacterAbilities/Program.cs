f00e008268068d25bff95849163627dee6a5b6ae
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 17:36:52 -0500

    Add files via upload

---
+++ Week 12/GameCharacterAbilities/Program.cs
@@ -0,0 +1,85 @@
+﻿
+
+using System.Diagnostics;
+
+System.Console.WriteLine("Welcome to the Game Character Abilities Program!");
+System.Console.WriteLine("Choose a character type to see their abilities:");
+System.Console.WriteLine("1. Warrior");
+System.Console.WriteLine("2. Mage");
+System.Console.WriteLine("3. Archer");
+System.Console.WriteLine("4. Healer");
+System.Console.Write("Enter your choice (1-4): ");
+int choice = int.Parse(Console.ReadLine());
+
+
+switch (choice) {
+    case 1:
+        Warrior warrior = new Warrior();
+        warrior.Attack();
+        warrior.Defend();
+        break;
+    case 2:
+        Mage mage = new Mage();
+        mage.Attack();
+        mage.Defend();
+        break;
+    case 3:
+        Archer archer = new Archer();
+        archer.Attack();
+        archer.Defend();
+        break;
+    case 4:
+        Healer healer = new Healer();
+        healer.Heal();
+        break;
+    default:
+        Console.WriteLine("Invalid choice. Please select a valid character type.");
+        break;
+}
+
+Console.ReadLine();
+
+public interface IAttackable {
+    void Attack();
+    void Defend();
+}
+
+public class Warrior : IAttackable {
+    public void Attack() {
+        Console.WriteLine("Warrior attacks with sword!");
+    }
+
+    public void Defend() {
+        Console.WriteLine("Warrior defends with shield!");
+    }
+}
+
+public class Mage : IAttackable {
+    public void Attack() {
+        Console.WriteLine("Mage casts a fireball!");
+    }
+
+    public void Defend() {
+        Console.WriteLine("Mage conjures a magic barrier!");
+    }
+}   
+
+public class Archer : IAttackable {
+    public void Attack() {
+        Console.WriteLine("Archer shoots an arrow!");
+    }
+
+    public void Defend() {
+        Console.WriteLine("Archer dodges the attack!");
+    }
+}
+
+public interface IHealable {
+    void Heal();
+}
+
+public class Healer : IHealable {
+    public void Heal() {
+        Console.WriteLine("Healer casts a healing spell!");
+    }
+}
---
+++ Week 12/InheritanceMiniProjectHomework/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace InheritanceMiniProjectHomework;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+            List<IRentable> rentables = new List<IRentable>();
+            List<IPurchasable> purchasables = new List<IPurchasable>();
+
+            BeachBall beachBall = new BeachBall { ProductName = "Beach Ball", QuantityInStock = 10 };
+            Towels towels = new Towels { ProductName = "Towels", QuantityInStock = 20 };
+            Umbrella umbrella = new Umbrella { ProductName = "Umbrella", QuantityInStock = 5 };
+            rentables.Add(umbrella);
+            purchasables.Add(beachBall);
+            purchasables.Add(towels);
+
+            Console.WriteLine("Welcome to the Beach Rental Store!");
+            System.Console.WriteLine("Do you want to rent or purchase an item? (Enter 'rent' or 'purchase')"); 
+            string itemToRent = Console.ReadLine();
+            if(itemToRent.ToLower() == "rent") 
+            {
+                Console.WriteLine("We have the following items available for rent:");
+
+                foreach (var item in rentables)
+                {
+                    Console.WriteLine($"{item.ProductName} - {item.QuantityInStock} available");
+                    System.Console.WriteLine("Do you want to rent this item? (yes/no)");
+                    string wantToRent = Console.ReadLine();
+                    if (wantToRent.ToLower() == "yes")
+                    {
+                        item.Rent();
+                        System.Console.WriteLine($"You have rented {item.ProductName}. Please return it when you are done.");
+                    }
+                    System.Console.WriteLine("Do you want to return this item? (yes/no)");
+                    string wantToReturn = Console.ReadLine();
+                    if (wantToReturn.ToLower() == "yes")
+                    {
+                        item.Return();
+                        System.Console.WriteLine($"You have returned {item.ProductName}. Thank you!");
+                    }
+                }
+
+            } else if(itemToRent.ToLower() == "purchase") 
+            {
+                Console.WriteLine("We have the following items available for purchase:");
+
+                foreach (var item in purchasables)
+                {
+                    Console.WriteLine($"{item.ProductName} - {item.QuantityInStock} available");
+                    System.Console.WriteLine("Do you want to purchase this item? (yes/no)");
+                    string wantToPurchase = Console.ReadLine();
+                    if (wantToPurchase.ToLower() == "yes")
+                    {
+                        item.Purchase();
+                        System.Console.WriteLine($"You have purchased {item.ProductName}. Thank you!");
+                    }
+                }
+            } else 
+            {
+                Console.WriteLine("Invalid input. Please enter 'rent' or 'purchase'.");
+            }
+
+
+    System.Console.WriteLine("Done");
+            Console.ReadLine(); 
+        }
+
+        public interface IInventoryItem
+        {
+            string ProductName { get; }
+
+            int QuantityInStock { get; }
+
+        }
+
+        public interface IRentable : IInventoryItem
+        {
+            void Rent();
+            void Return();
+        }
+
+        public interface IPurchasable : IInventoryItem
+        {
+            void Purchase();
+        }
+        public class InventoryItem : IInventoryItem
+        {
+            public string ProductName { get; set; }
+            public int QuantityInStock { get; set; }
+        }
+
+        public class BeachBall : InventoryItem, IPurchasable 
+        {
+            public void Purchase()
+            {
+                QuantityInStock -= 1;
+                Console.WriteLine($"Purchasing {ProductName}");
+            }
+        }
+
+
+        public class Towels: InventoryItem,  IPurchasable
+        {
+            public void Purchase()
+            {
+                QuantityInStock -= 1;
+                Console.WriteLine($"Purchasing {ProductName}");
+            }
+        }
+        
+
+        public class Umbrella: InventoryItem, IRentable
+        {
+            public void Rent()
+            {
+                QuantityInStock -= 1;
+                Console.WriteLine($"Renting {ProductName}");
+            }
+
+            public void Return()
+            {
+                QuantityInStock += 1;
+                Console.WriteLine($"Returning {ProductName}");
+            }
+        }
+       
+  }  
