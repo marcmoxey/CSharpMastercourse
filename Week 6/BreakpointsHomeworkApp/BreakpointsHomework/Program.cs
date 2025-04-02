bc1c6efd85e8688d70684a2018bddcd8f971119c
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 16:08:22 -0500

    Add files via upload

---
+++ Week 6/BreakpointsHomeworkApp/BreakpointsHomework/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+/* 
+ * 
+ * For loop that multiples a number by 5
+ * and add its to the total each time
+ * time through the code
+ * 
+*/
+
+int total = 0;
+for (int i = 1; i < 10; i++)
+{
+    total += i * 5;
+    Console.WriteLine($"The current total is {total}");
+}
+Console.WriteLine($"The total is {total}");
