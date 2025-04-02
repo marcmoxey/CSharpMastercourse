dc0a38381ecdcbacc81f7dd855f3b6693f3d7937
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 16:09:40 -0500

    Add files via upload

---
+++ NamespaceHomeworkApp/ConsoleUI/UserMessages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMessages
+{
+    public class UserMessages
+    {
+        public static void PrintResults(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ PropertyTypeDemo/ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel("Moxey");
+
+            person.FirstName = "Marc";
+           // person.LastName = "Moxey";
+            person.Age = 23;
+            person.SSN = "123-45-6789";
+
+            Console.WriteLine(person.FullName);
+
+            Console.WriteLine(person.SSN);
+            Console.ReadLine(); 
+        }
+    }
+}
