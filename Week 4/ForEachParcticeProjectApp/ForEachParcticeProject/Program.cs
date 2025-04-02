5d9de65b5ef5ff577a496994b3011f6320ea8959
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 16:04:22 -0500

    Add files via upload

---
+++ Week 4/ForEachParcticeProjectApp/ForEachParcticeProject/Program.cs
@@ -0,0 +1,10 @@
+﻿
+List<string> firstNames = new();
+firstNames.Add("Nio");
+firstNames.Add("Marc");
+firstNames.Add("Jon");
+
+foreach (string name in firstNames)
+{
+    Console.WriteLine(name);
+}
---
+++ Week 4/ForLoopsApp/ForLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"The value of i is {i}");
+//}
+
+//string data = "Tim,Sue,Bob,Jane";
+//List<string> firstName = data.Split(',').ToList();
+//for (int i = 0; i < firstName.Count; i++)
+//{
+//    Console.WriteLine($"{firstName[i]} is in attendance");
+//}
+
+List<decimal> charges = new ();
+charges.Add(23.78M);
+charges.Add(10.99M);
+charges.Add(15M);
+
+decimal total = 0;
+for (int i = 0; i < charges.Count; i++)
+{
+    total += charges[i];
+}
+Console.WriteLine($"Our total charge is {total}");
