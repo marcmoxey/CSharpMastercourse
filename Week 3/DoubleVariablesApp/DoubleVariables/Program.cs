34b9a5ac7d52f73e145eb5c1d9252d6f0d46e276
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 15:58:32 -0500

    Add files via upload

---
+++ Week 3/DoubleVariablesApp/DoubleVariables/Program.cs
@@ -0,0 +1,10 @@
+﻿// doubles can have a decimal point - 1.23, 43
+
+double averageAge;
+
+//averageAge = (43.0 + 22 + 62) / 3; // int are assume if their a whole number
+
+averageAge = 2.0 / 3;
+Console.WriteLine(averageAge);
+
+Console.ReadLine();
