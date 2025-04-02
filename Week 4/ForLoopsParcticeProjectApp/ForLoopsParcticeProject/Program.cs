6efa1a5bf098cc46e00d63fd9eed340843cf83f5
Marc Moxey <moxeymarc@gmail.com>
2025-04-02 16:05:05 -0500

    Add files via upload

---
+++ Week 4/ForLoopsParcticeProjectApp/ForLoopsParcticeProject/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Net.Http.Headers;
+
+List<string> firstNames = new List<string>();
+firstNames.Add("Marc");
+firstNames.Add("Jon");
+firstNames.Add("Nio");
+
+for(int i = 0; i < firstNames.Count; i++)
+{
+    Console.WriteLine(firstNames[i]);
+}
+
+
+for(int i = 0; i < 100; i+=2)
+{
+    Console.WriteLine(i);
+}
---
+++ Week 4/GuestBookMiniProject/GuestBook/PartyMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook
+{
+    public static class PartyMethods
+    {
+        public static void WelcomeUser()
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Welcome To Guest Book Application");
+            
+        }
+
+    }
+}
