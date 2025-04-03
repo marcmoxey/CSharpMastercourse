926df72eb1b5bb3b2ecf704d8f48c722e774a75b
Marc Moxey <moxeymarc@gmail.com>
2025-04-03 11:41:52 -0500

    Add files via upload

---
+++ Week 14/AccessModifiersDemoApp/DemoLibrary/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoLibrary
+{
+    public class Employee : Person
+    {
+        public string GetFormerLastName()
+        {
+            // protected allow access something in any class that derives from this class
+            return formerLastName;
+        }
+    }
+}
