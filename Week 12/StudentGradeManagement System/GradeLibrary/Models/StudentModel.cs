3eccbe49772a18b4ad56c2f97034e750de4f38d7
Marc Moxey <moxeymarc@gmail.com>
2025-04-03 12:20:25 -0500

    Add files via upload

---
+++ Week 12/StudentGradeManagement System/GradeLibrary/Models/StudentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeLibrary.Models
+{
+    public class StudentModel
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public Dictionary<Subjects, double> Grades { get; set; } = new Dictionary<Subjects, double>();
+
+        public StudentModel(int Id, string name)
+        {
+            StudentId = Id;
+            StudentName = name;
+          
+        }
+    }
+}
