244c3a7c21de6c9aeeb59cf28123fb33c64544ac
Marc Moxey <moxeymarc@gmail.com>
2025-04-08 13:10:47 -0500

    Add files via upload

---
+++ Week 14/AbstractClassHomeworkApp/DemoLibrary/IDrive.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public interface IDrive
+    {
+        void Start();
+        void Stop();
+        void Accelerate(int speed);
+        void Brake(int speed);
+        void Turn(string direction);
+    }
+}
