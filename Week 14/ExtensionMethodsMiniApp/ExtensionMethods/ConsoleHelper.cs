d45a50b8809f5d3d9dc452222121c50864e1d829
Marc Moxey <moxeymarc@gmail.com>
2025-04-10 12:27:48 -0500

    Add files via upload

---
+++ Week 14/ExtensionMethodsMiniApp/ExtensionMethods/ConsoleHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+
+            string output = string.Empty;
+
+            while (string.IsNullOrEmpty(output))
+            {
+                
+                Console.Write(message);
+                output = Console.ReadLine();
+            }
+
+            return output;  
+        }
+
+        public static int RequestInt(this string message, int minValue, int maxValue)
+        {
+            return message.RequestInt(true, minValue, maxValue);
+        }
+
+        public static int RequestInt(this string message)
+        {
+           return message.RequestInt(false);
+        }
+
+        private static int RequestInt(this string message, bool useMinMax, int minValue = 0, int maxValue = 0)
+        {
+            int output = 0;
+            bool isValid = false;
+            bool isInValidRange = true;
+
+            while (isValid == false || isInValidRange == false)
+            {
+                Console.Write(message);
+                isValid = int.TryParse(Console.ReadLine(), out output);
+
+                if (useMinMax == true)
+                {
+                    if (output >= minValue && output <= maxValue)
+                    {
+                        isInValidRange = true;
+                    }
+                    else
+                    {
+                        isInValidRange = false;
+
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ Week 14/ExtensionMethodsMiniApp/ExtensionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+            
+            
+            person.FirstName = "What is your first name: ".RequestString();
+            
+            person.LastName = "What is your last name: ".RequestString();
+
+            person.Age = "How old are you: ".RequestInt(0,120);
+
+            Console.ReadLine();
+        }
+    }
+}
