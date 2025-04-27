166473c3b4a921322cdc5ba9e481dc121271f9a8
Marc Moxey <moxeymarc@gmail.com>
2025-04-27 17:35:12 -0500

    Add files via upload

---
+++ Week 17/BrokenLibrary/CompanyModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BrokenLibrary
+{
+    public class CompanyModel
+    {
+        public string CompanyName { get; set; }
+        public int NumberOfEmployees { get; set; }
+        public int NumberOfDaysOffTotal { get; set; }
+        
+        public int AverageDaysOff ()
+        {
+            return NumberOfDaysOffTotal / NumberOfEmployees;
+        }
+    }
+}
