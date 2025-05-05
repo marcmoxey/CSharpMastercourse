10bb06174157054c081f29738a0c87896cc19fc8
Marc Moxey <moxeymarc@gmail.com>
2025-05-05 10:35:01 -0500

    Add files via upload

---
+++ Week 19/ProductInventoryManagmentApp/ProductLibrary/Logic/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ProductLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductLibrary.Enums;
+
+namespace ProductLibrary.Logic
+{
+    public class ProductService
+    {
+        public bool ValidateProduct(ProductModel product)
+        {
+            return !string.IsNullOrWhiteSpace(product.ProductName) && !string.IsNullOrWhiteSpace(product.Categories.ToString()) && product.Price > 0 && product.Suppliers.Count > 0;
+        }
+
+        public bool ValidateSupplier(SupplierModel supplier)
+        {
+            return !string.IsNullOrWhiteSpace(supplier.SupplierName) && !string.IsNullOrWhiteSpace(supplier.ContactNumber);
+        }
+    }
+}
