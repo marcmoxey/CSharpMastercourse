1608e36d8c31ba80eaf1e224e668e7aebd2edf76
Marc Moxey <moxeymarc@gmail.com>
2025-05-05 07:27:28 -0500

    Add files via upload

---
+++ Week 19/StudentEnrollmentApp/StudentCourseEnrollment/StudentEntry.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace StudentCourseEnrollment
+{
+    partial class StudentEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentEntryLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.courseListBox = new System.Windows.Forms.ListBox();
+            this.courseLabel = new System.Windows.Forms.Label();
+            this.addCourseBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // studentEntryLabel
+            // 
+            this.studentEntryLabel.AutoSize = true;
+            this.studentEntryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentEntryLabel.Location = new System.Drawing.Point(39, 20);
+            this.studentEntryLabel.Name = "studentEntryLabel";
+            this.studentEntryLabel.Size = new System.Drawing.Size(240, 39);
+            this.studentEntryLabel.TabIndex = 0;
+            this.studentEntryLabel.Text = "Student Entry";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(64, 92);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(131, 29);
+            this.firstNameLabel.TabIndex = 1;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(64, 169);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(128, 29);
+            this.lastNameLabel.TabIndex = 2;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.Location = new System.Drawing.Point(201, 92);
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.Size = new System.Drawing.Size(239, 35);
+            this.firstNameText.TabIndex = 1;
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(201, 163);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(239, 35);
+            this.lastNameText.TabIndex = 2;
+            // 
+            // courseListBox
+            // 
+            this.courseListBox.FormattingEnabled = true;
+            this.courseListBox.ItemHeight = 29;
+            this.courseListBox.Location = new System.Drawing.Point(51, 268);
+            this.courseListBox.Name = "courseListBox";
+            this.courseListBox.Size = new System.Drawing.Size(389, 178);
+            this.courseListBox.TabIndex = 5;
+            this.courseListBox.TabStop = false;
+            // 
+            // courseLabel
+            // 
+            this.courseLabel.AutoSize = true;
+            this.courseLabel.Location = new System.Drawing.Point(64, 236);
+            this.courseLabel.Name = "courseLabel";
+            this.courseLabel.Size = new System.Drawing.Size(103, 29);
+            this.courseLabel.TabIndex = 6;
+            this.courseLabel.Text = "Courses";
+            // 
+            // addCourseBtn
+            // 
+            this.addCourseBtn.Location = new System.Drawing.Point(338, 224);
+            this.addCourseBtn.Name = "addCourseBtn";
+            this.addCourseBtn.Size = new System.Drawing.Size(102, 41);
+            this.addCourseBtn.TabIndex = 3;
+            this.addCourseBtn.Text = "Add";
+            this.addCourseBtn.UseVisualStyleBackColor = true;
+            this.addCourseBtn.Click += new System.EventHandler(this.addCourseBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(135, 483);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(144, 59);
+            this.saveBtn.TabIndex = 4;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // StudentEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(510, 576);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.addCourseBtn);
+            this.Controls.Add(this.courseLabel);
+            this.Controls.Add(this.courseListBox);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.firstNameText);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.studentEntryLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "StudentEntry";
+            this.Text = "StudentEntry";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label studentEntryLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox firstNameText;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.ListBox courseListBox;
+        private System.Windows.Forms.Label courseLabel;
+        private System.Windows.Forms.Button addCourseBtn;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
