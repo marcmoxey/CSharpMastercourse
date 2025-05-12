10b900bc68a44f10a0105bf7e6be727a223a7cdc
Marc Moxey <moxeymarc@gmail.com>
2025-05-12 07:35:08 -0500

    Add files via upload

---
+++ Week 20/WPFMiniProjectApp/WPFMiniProject/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using DemoLibrary.Logic;
+using DemoLibrary.Models;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMiniProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, ISaveAddress
+    {
+        BindingList<AddressModel> addresses = new BindingList<AddressModel>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            addressesListBox.ItemsSource = addresses;
+           
+        }
+
+        public void SaveAddress(AddressModel address)
+        {
+            addresses.Add(address);
+        }
+
+        private void addAddressBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddressEntry entry = new AddressEntry(this);
+            entry.Show();
+            
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = firstNameTextBox.Text,
+                LastName = lasttNameTextBox.Text,
+                Addresses = addresses.ToList()
+            };
+        }
+    }
+}
