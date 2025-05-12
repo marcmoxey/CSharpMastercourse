5fe87ece1fe9940d82304717ce4934fd87356124
Marc Moxey <moxeymarc@gmail.com>
2025-05-12 07:36:25 -0500

    Add files via upload

---
+++ Week 20/DemoLibrary/Logic/ISaveAddress.cs
@@ -0,0 +1,14 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Logic
+{
+    public interface ISaveAddress
+    {
+        void SaveAddress(AddressModel address);
+    }
+}
