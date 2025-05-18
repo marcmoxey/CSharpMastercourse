9e6ca562ba4b79c549dfe0a1520ee498281f8ad9
Marc Moxey <moxeymarc@gmail.com>
2025-05-18 16:09:15 -0500

    Add files via upload

---
+++ Week 22/UserLibrary.Tests/PersonExtensionsTests.cs
@@ -0,0 +1,124 @@
+using UserLibrary.Models;
+using UserLibrary.BusinessLogic;
+using Xunit;
+using System;
+
+namespace UserLibrary.Tests
+{
+    public class PersonExtensionsTests
+    {
+        [Theory]
+        [InlineData("Tim", "Corey", "Tim Corey")]
+        [InlineData("Jane", "Smith", "Jane Smith")]
+        [InlineData("Sue", "Storm", "Sue Storm")]
+        public void GetFullNameShouldReturnValidNames(string firstName,
+                                                      string lastName,
+                                                      string expected)
+        {
+            // Arrange
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+
+            // Act
+            string actual = p.GetFullName();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Tim", "")]
+        [InlineData("", "Smith")]
+        [InlineData("", "")]
+        [InlineData("Tim", "   ")]
+        [InlineData("   ", "Smith")]
+        [InlineData("   ", "   ")]
+        [InlineData("Tim", null)]
+        [InlineData(null, "Smith")]
+        [InlineData(null, null)]
+        public void GetFullNameShouldThrowExceptions(string firstName,
+                                                      string lastName)
+        {
+            // Arrange
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+
+            // Act
+            Assert.Throws<ArgumentException>(() => p.GetFullName());
+        }
+
+        [Theory]
+        [InlineData("Tim", "Corey", "TCorey")]
+        [InlineData("Jane", "Smith", "JSmith")]
+        [InlineData("Sue", "Storm", "SStorm")]
+        public void GetLoginNameShouldReturnValidNames(string firstName,
+                                                      string lastName,
+                                                      string expected)
+        {
+            // Arrange
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+
+            // Act
+            string actual = p.GetLoginName();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Tim", "")]
+        [InlineData("", "Smith")]
+        [InlineData("", "")]
+        [InlineData("Tim", "   ")]
+        [InlineData("   ", "Smith")]
+        [InlineData("   ", "   ")]
+        [InlineData("Tim", null)]
+        [InlineData(null, "Smith")]
+        [InlineData(null, null)]
+        public void GetLoginNameShouldThrowExceptions(string firstName,
+                                                      string lastName)
+        {
+            // Arrange
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+
+            // Act
+            Assert.Throws<ArgumentException>(() => p.GetLoginName());
+        }
+
+        [Theory]
+        [InlineData("Tim", "Corey", "TC")]
+        [InlineData("Jane", "Smith", "JS")]
+        [InlineData("Sue", "Storm", "SS")]
+        public void GetInitialsShouldReturnValidNames(string firstName,
+                                                      string lastName,
+                                                      string expected)
+        {
+            // Arrange
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+
+            // Act
+            string actual = p.GetInitials();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Tim", "")]
+        [InlineData("", "Smith")]
+        [InlineData("", "")]
+        [InlineData("Tim", "   ")]
+        [InlineData("   ", "Smith")]
+        [InlineData("   ", "   ")]
+        [InlineData("Tim", null)]
+        [InlineData(null, "Smith")]
+        [InlineData(null, null)]
+        public void GetInitialsShouldThrowExceptions(string firstName,
+                                                      string lastName)
+        {
+            // Arrange
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+
+            // Act
+            Assert.Throws<ArgumentException>(() => p.GetInitials());
+        }
+    }
+}
---
+++ Week 22/WPFUI/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using UserLibrary.BusinessLogic;
+using UserLibrary.Models;
+
+namespace WPFUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void processForm_Click(object sender, RoutedEventArgs e)
+        {
+            errorLabel.Visibility = Visibility.Hidden;
+
+            if (string.IsNullOrWhiteSpace(firstNameText.Text))
+            {
+                errorLabel.Text = "You need to provide a valid first name.";
+                errorLabel.Visibility = Visibility.Visible;
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(lastNameText.Text))
+            {
+                errorLabel.Text = "You need to provide a valid last name.";
+                errorLabel.Visibility = Visibility.Visible;
+                return;
+            }
+
+            PersonModel person = new();
+            person.FirstName = firstNameText.Text;
+            person.LastName = lastNameText.Text;
+
+            fullNameLabel.Text = "Full Name: " + person.GetFullName();
+            loginNameLabel.Text = "Login Name: " + person.GetLoginName();
+            initialsLabel.Text = "Initials: " + person.GetInitials();
+        }
+    }
+}
