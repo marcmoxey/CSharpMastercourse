5a056e773c2d771d282613a1788ad16bba232533
Marc Moxey <moxeymarc@gmail.com>
2025-06-16 11:13:28 -0400

    Add Data Entry form for address and Person

---
+++ Week 26/BlazorAppMiniProjectApp/BlazorAppMiniProject/Models/AddressModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppMiniProject.Models
+{
+    public class AddressModel
+    {
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        [StringLength(maximumLength: 10, MinimumLength = 5)]
+        public string ZipCode { get; set; }     
+    }
+}
