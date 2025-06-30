f9bb0cbc59f85981c3ec57d61e79349ef25f1fff
Marc Moxey <moxeymarc@gmail.com>
2025-06-30 10:55:31 -0400

    Add Homework for BlazorWasm

---
+++ Week 29/BlazorWasmhomeworkApp/BlazorWasmhomework/Models/PersonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorWasmhomework.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }   
+        public string LastName { get; set; } 
+    }
+}
