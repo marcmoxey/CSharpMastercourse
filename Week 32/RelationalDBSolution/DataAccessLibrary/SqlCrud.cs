943f210853d50fb2865f07725731a0a2949eb718
Marc Moxey <moxeymarc@gmail.com>
2025-08-04 15:07:28 -0400

    Add method to get contact by id

--- Week 32/RelationalDBSolution/DataAccessLibrary/SqlCrud.cs
+++ Week 32/RelationalDBSolution/DataAccessLibrary/SqlCrud.cs
@@ -23,17 +23,37 @@ namespace DataAccessLibrary
             return db.LoadData<BasicContactModel, dynamic>(sql, new { }, _connectionString);
         }
 
-        public List<FullContactModel> GetFullContctById(int id)
+        public FullContactModel GetFullContactById(int id)
         {
             string sql = "select Id, FirstName, LastName from dbo.Contacts where Id = @Id";
             FullContactModel output = new FullContactModel();
             output.BasicInfo = db.LoadData<BasicContactModel, dynamic>(sql, new { Id = id }, _connectionString).FirstOrDefault(); // return first record
 
-            if(output.BasicInfo != null)
+            if(output.BasicInfo == null)
             {
                 // do something to tell the user that the record was not found
                 throw new Exception("User not found");
+                //return null;
             }
+
+            sql = @"select e.*
+                    from dbo.EmailAddresses e
+                    inner join dbo.ContactEmail ce on ce.EmailAddressId = e.Id
+                    where ce.ContactId = @Id";
+
+            // load all the email addresses 
+            output.EmailAddresses = db.LoadData<EmailAddressModel, dynamic>(sql, new { Id = id }, _connectionString);
+
+            // load all the phone numbers
+            sql = @"select p.*
+                    from dbo.PhoneNumbers p
+                    inner join dbo.ContactPhoneNumber cpn on cpn.PhoneNumberId = p.Id
+                    where cpn.ContactId = @Id";
+
+
+            output.PhoneNumbers = db.LoadData<PhoneNumberModel, dynamic>(sql, new{ Id = id }, _connectionString);
+
+            return output;
         }
     }
 }
--- Week 32/RelationalDBSolution/SQLServerUI/Program.cs
+++ Week 32/RelationalDBSolution/SQLServerUI/Program.cs
@@ -6,7 +6,8 @@ using Microsoft.Extensions.Configuration;
 //Console.WriteLine(GetConnectionString());
 
 SqlCrud sql = new SqlCrud(GetConnectionString());
-ReadAllContacts(sql);
+//ReadAllContacts(sql);
+ReadContact(sql, 1);
 Console.ReadLine();
 
 static void ReadAllContacts(SqlCrud sql)
@@ -18,6 +19,14 @@ static void ReadAllContacts(SqlCrud sql)
     }
 }
 
+static void ReadContact(SqlCrud sql, int ContactId)
+{
+    var contact = sql.GetFullContactById(ContactId);
+    
+  
+    Console.WriteLine($"{contact.BasicInfo.Id} {contact.BasicInfo.FirstName} {contact.BasicInfo.LastName}");
+
+}
 static string GetConnectionString(string connectionStringName = "Default")
 {
     string output = "";
