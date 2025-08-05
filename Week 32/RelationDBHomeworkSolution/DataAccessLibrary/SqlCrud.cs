b222ac5343dc273dfd80c2c4c7488032054f4b12
Marc <moxiem@nychhc.org>
2025-08-05 13:33:32 -0400

    Add setup

--- Week 32/RelationDBHomeworkSolution/DataAccessLibrary/SqlCrud.cs
+++ Week 32/RelationDBHomeworkSolution/DataAccessLibrary/SqlCrud.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DataAccessLibrary.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,94 @@ namespace DataAccessLibrary
 {
     public class SqlCrud
     {
+        private readonly string _connectionString;
+
+        public SqlCrud(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<PersonModel> GetAllPeople()
+        {
+
+            string sql = "select Id, FirstName, LastName from dbo.People";
+           List<PersonModel> people = new List<PersonModel>();
+            using (SqlConnection connection = new(_connectionString))
+            {
+                SqlCommand cmd = new(sql,connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    
+                    while(reader.Read())
+                    {
+                        PersonModel person = new PersonModel
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            FirstName = reader["FirstName"].ToString(),
+                            LastName = reader["LastName"].ToString()
+                        };
+
+                        
+                        people.Add(person);
+
+                    }
+                    reader.Close();
+                 
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            
+            }
+
+            return people;
+        }
+
+        public PersonModel GetPersonById(int id)
+        {
+            PersonModel person = new PersonModel();
+            string sql = "select Id, FirstName, LastName from dbo.People where Id = @Id"; 
+            using (SqlConnection connection = new(_connectionString))
+            {
+                SqlCommand cmd = new(sql, connection);
+                SqlParameter param = new SqlParameter("@Id", id);
+              
+                cmd.Parameters.Add(param);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        person = new PersonModel
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            FirstName = reader["FirstName"].ToString(),
+                            LastName = reader["LastName"].ToString()
+                        };
+
+
+                      
+
+                    }
+                    reader.Close();
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            return person;
+        }
     }
 }
--- Week 32/RelationDBHomeworkSolution/SqlServerHomework/Program.cs
+++ Week 32/RelationDBHomeworkSolution/SqlServerHomework/Program.cs
@@ -1,4 +1,42 @@
-﻿
+﻿using DataAccessLibrary;
+using Microsoft.Extensions.Configuration;
+
+SqlCrud sql = new SqlCrud(GetConnectionString());
+
+//GetAllPeople(sql);
+//GetPersonById(sql, 1);
+
+Console.WriteLine("Done!");
+Console.ReadLine();
 
 
-Console.ReadLine(); 
+static void GetPersonById(SqlCrud sql, int id)
+{
+    var Person = sql.GetPersonById(id);
+
+    Console.WriteLine($"{Person.Id}: {Person.FirstName}, {Person.LastName}");
+}
+static void GetAllPeople(SqlCrud sql)
+{
+    var rows = sql.GetAllPeople();
+    foreach(var row in rows)
+    {
+        Console.WriteLine($"{row.Id}.{row.FirstName}, {row.LastName}");
+    }
+}
+
+
+static string GetConnectionString(string connectionStringName = "Default")
+{
+    string output = "";
+
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+
+    var config = builder.Build();
+
+    output = config.GetConnectionString(connectionStringName);
+
+    return output;
+}
