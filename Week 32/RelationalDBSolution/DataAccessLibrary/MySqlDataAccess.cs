101af504be21901a0ec7d4a5f35b2177a21eab7c
Marc Moxey <moxeymarc@gmail.com>
2025-08-09 18:27:03 -0400

    Add MySql

---
+++ Week 32/RelationalDBSolution/DataAccessLibrary/MySqlDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class MySqlDataAccess
+    {
+        public List<T> LoadData<T, U>(string sqlStatment, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatment, parameters).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatment, T parameter, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Execute(sqlStatment, parameter);
+            }
+        }
+    }
+}
--- Week 32/RelationalDBSolution/MYSQLUI/Program.cs
+++ Week 32/RelationalDBSolution/MYSQLUI/Program.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
+
+
+MySqlCrud sql = new MySqlCrud(GetConnectionString());
+
+//ReadAllContacts(sql);
+
+//ReadContact(sql, 2);
+
+//CreateNewContact(sql);
+
+//UpdateContact(sql);
+//ReadAllContacts(sql);
+
+//RemovePhoneNumberFromContact(sql,2, 1);
+
+Console.WriteLine("Done! MySQL");
+
+Console.ReadLine();
+
+
+static void RemovePhoneNumberFromContact(MySqlCrud sql, int contactId, int phoneNumberId)
+{
+    sql.RemovePhoneNumberFromContact(contactId, phoneNumberId);
+}
+
+static void UpdateContact(MySqlCrud sql)
+{
+    BasicContactModel contact = new BasicContactModel
+    {
+        Id = 1,
+        FirstName = "Marc-Anthony",
+        LastName = "Moxey"
+    };
+    sql.UpdateContactName(contact);
+}
+
+static void CreateNewContact(MySqlCrud sql)
+{
+    FullContactModel user = new FullContactModel
+    {
+        BasicInfo = new BasicContactModel
+        {
+            FirstName = "Charity",
+            LastName = "Corey"
+        }
+    };
+    user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "nope@aol.com" });
+    user.EmailAddresses.Add(new EmailAddressModel { id = 2, EmailAddress = "me@timothycorey.com" });
+
+    user.PhoneNumbers.Add(new PhoneNumberModel { Id = 1, PhoneNumber = "555-1212" });
+    user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-9876" });
+
+    sql.CreateContact(user);
+}
+
+static void ReadAllContacts(MySqlCrud sql)
+{
+    var rows = sql.GetAllContacts();
+    foreach (var row in rows)
+    {
+        Console.WriteLine($"{row.Id} {row.FirstName} {row.LastName}");
+    }
+}
+
+static void ReadContact(MySqlCrud sql, int ContactId)
+{
+    var contact = sql.GetFullContactById(ContactId);
+
+
+    Console.WriteLine($"{contact.BasicInfo.Id}: {contact.BasicInfo.FirstName} {contact.BasicInfo.LastName}");
+
+}
+static string GetConnectionString(string connectionStringName = "Default")
+{
+    string output = "";
+
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+
+    var config = builder.Build();
+
+    output = config.GetConnectionString(connectionStringName);
+
+    return output;
+}
