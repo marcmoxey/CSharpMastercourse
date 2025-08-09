90a613e8beeef950aebd00fb0b660a133ff1b1eb
Marc Moxey <moxeymarc@gmail.com>
2025-08-09 14:54:18 -0400

    Add SQLite

---
+++ Week 32/RelationalDBSolution/SqliteUI/Program.cs
@@ -0,0 +1,89 @@
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
+
+
+SqliteCrud sql = new SqliteCrud(GetConnectionString());
+
+//ReadAllContacts(sql);
+
+//ReadContact(sql, 3);
+
+//CreateNewContact(sql);
+
+//UpdateContact(sql);
+//ReadAllContacts(sql);
+
+RemovePhoneNumberFromContact(sql,2, 1);
+
+Console.WriteLine("Done! Sqlite");
+
+Console.ReadLine();
+
+
+static void RemovePhoneNumberFromContact(SqliteCrud sql, int contactId, int phoneNumberId)
+{
+    sql.RemovePhoneNumberFromContact(contactId, phoneNumberId);
+}
+
+static void UpdateContact(SqliteCrud sql)
+{
+    BasicContactModel contact = new BasicContactModel
+    {
+        Id = 1,
+        FirstName = "Marc-Anthony",
+        LastName = "Moxey"
+    };
+    sql.UpdateContactName(contact);
+}
+
+static void CreateNewContact(SqliteCrud sql)
+{
+    FullContactModel user = new FullContactModel
+    {
+        BasicInfo = new BasicContactModel
+        {
+            FirstName = "Charity",
+            LastName = "Corey"
+        }
+    };
+    user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "nope@aol.com" });
+    user.EmailAddresses.Add(new EmailAddressModel { id = 2, EmailAddress = "me@timothycorey.com" });
+
+    user.PhoneNumbers.Add(new PhoneNumberModel { Id = 1, PhoneNumber = "555-1212" });
+    user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-9876" });
+
+    sql.CreateContact(user);
+}
+
+static void ReadAllContacts(SqliteCrud sql)
+{
+    var rows = sql.GetAllContacts();
+    foreach (var row in rows)
+    {
+        Console.WriteLine($"{row.Id} {row.FirstName} {row.LastName}");
+    }
+}
+
+static void ReadContact(SqliteCrud sql, int ContactId)
+{
+    var contact = sql.GetFullContactById(ContactId);
+
+
+    Console.WriteLine($"{contact.BasicInfo.Id}: {contact.BasicInfo.FirstName} {contact.BasicInfo.LastName}");
+
+}
+static string GetConnectionString(string connectionStringName = "Default")
+{
+    string output = "";
+
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+
+    var config = builder.Build();
+
+    output = config.GetConnectionString(connectionStringName);
+
+    return output;
+}
