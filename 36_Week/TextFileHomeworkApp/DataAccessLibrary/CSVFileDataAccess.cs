608d109b6c4c605e978715c9de8dae1a1c6c6de6
Marc <moxiem@nychhc.org>
2025-08-19 10:37:56 -0400

    Add read and write to csv file

---
+++ 36_Week/TextFileHomeworkApp/DataAccessLibrary/CSVFileDataAccess.cs
@@ -0,0 +1,56 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class CSVFileDataAccess
+    {
+        public List<PersonModel> ReadAllRecords(string csvFile)
+        {
+            if(File.Exists(csvFile) == false)
+            {
+                return new List<PersonModel>();
+            }
+
+            var lines = File.ReadAllLines(csvFile);
+            List<PersonModel> output = new List<PersonModel>();
+
+            foreach(var line in lines)
+            {
+                PersonModel person = new PersonModel();
+                var vals = line.Split(',');
+
+                if(vals.Length > 2)
+                {
+                    throw new Exception($"Invalid row data: {line}");
+                }
+        
+               
+                person.FirstName = vals[0];
+                person.LastName = vals[1];
+
+               
+        
+                
+
+                output.Add(person);
+            }
+            return output;
+        }
+
+        public void WrtieAllRecords(List<PersonModel> people, string csvFile)
+        {
+            List<string> lines = new();
+
+            foreach(var perosn in people)
+            {
+                lines.Add($"{perosn.FirstName},{perosn.LastName}");
+            }
+            File.WriteAllLines(csvFile, lines);
+        }
+    }
+}
---
+++ 36_Week/TextFileHomeworkApp/TextFileHomework/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace TextFileHomework
+{
+    public class Program
+    {
+        private static IConfiguration _config;
+        private static string csvFile;
+        private static CSVFileDataAccess db = new CSVFileDataAccess();
+
+        static void Main(string[] args)
+        {
+            InitializeConfiguration();
+            csvFile = _config.GetValue<string>("CSVFile");
+
+
+
+            PersonModel person1 = new PersonModel
+            {
+                FirstName = "Marc",
+                LastName = "Moxey",
+               
+             };
+
+            PersonModel person2 = new PersonModel
+            {
+                FirstName = "Sue",
+                LastName = "Storm",
+              
+            };
+
+            CreatePerson(person1);
+            CreatePerson(person2);
+            GetAllPeople();
+
+
+
+
+
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        private static void GetAllPeople()
+        {
+            var people = db.ReadAllRecords(csvFile);
+            foreach(var person in people)
+            {
+                Console.WriteLine($"{person.FirstName}, {person.LastName}");
+            }
+        }
+        private static void CreatePerson(PersonModel person)
+        {
+            var people = db.ReadAllRecords(csvFile);
+            people.Add(person);
+            db.WrtieAllRecords(people, csvFile);
+        }
+        private static void InitializeConfiguration()
+        {
+
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json");
+
+            _config = builder.Build();
+
+        }
+    }
+}
