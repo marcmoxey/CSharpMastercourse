8ee5b2aedbe918730dfddfc75317cfee093506b3
Marc <moxiem@nychhc.org>
2025-08-19 09:34:29 -0400

    Add read and write to text file

---
+++ 36_Week/TextFileSolution/DataAccessLibrary/TextFileDataAccess.cs
@@ -0,0 +1,56 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class TextFileDataAccess
+    {
+        public List<ContactModel> ReadAllRecords(string textFile)
+        {
+            if (File.Exists(textFile) == false)
+            {
+                return new List<ContactModel>();
+            }
+
+            var lines = File.ReadAllLines(textFile);
+            List<ContactModel> output = new List<ContactModel>();
+
+            foreach (var line in lines)
+            {
+                ContactModel c = new ContactModel();
+                var vals = line.Split(','); // spit string into a string array
+               
+                if(vals.Length < 4) // check if line is valid
+                {
+                    throw new Exception($"Invalid row data: {line}");
+                }
+                c.FirstName = vals[0];
+                c.LastName = vals[1];
+                c.EmailAddresses = vals[2].Split(';').ToList();
+                c.PhoneNumbers = vals[3].Split(';').ToList();
+
+                output.Add(c);
+            }
+
+            return output;
+        }
+
+        public void WriteAllRecords(List<ContactModel> contacts, string textFile)
+        {
+            // Tim,Corey,555-1212
+            List<string> lines = new();
+
+            foreach(var c in contacts)
+            {
+                lines.Add($"{c.FirstName},{c.LastName},{String.Join(';', c.EmailAddresses)},{String.Join(';', c.PhoneNumbers)}");
+            }
+            File.WriteAllLines(textFile, lines);
+        }
+
+
+    }
+}
---
+++ 36_Week/TextFileSolution/TextFileUI/Program.cs
@@ -0,0 +1,113 @@
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+
+namespace TextFileUI
+{
+    class Program
+    {
+        private static IConfiguration _config;
+        private static string textFile;
+        private static TextFileDataAccess db = new TextFileDataAccess();
+
+        static void Main(string[] args)
+        {
+            InitializeConfiguration();
+            textFile = _config.GetValue<string>("TextFile");
+
+
+            ContactModel user1 = new ContactModel();
+            user1.FirstName = "Tim";
+            user1.LastName = "Corey";
+            user1.EmailAddresses.Add("tim@iamtimcorey.com");
+            user1.EmailAddresses.Add("me@timothycorey.com");
+            user1.PhoneNumbers.Add("555-1212");
+            user1.PhoneNumbers.Add("555-1234");
+
+            ContactModel user2 = new ContactModel();
+            user2.FirstName = "Charity";
+            user2.LastName = "Corey";
+            user2.EmailAddresses.Add("nope@aol.com");
+            user2.EmailAddresses.Add("me@timothycorey.com");
+            user2.PhoneNumbers.Add("555-1212");
+            user2.PhoneNumbers.Add("555-9876");
+
+
+            //CreateContact(user1);
+            //CreateContact(user2);
+            //GetAllContacts();
+            //UpdatecContactFirstName("Timothy");
+            //GetAllContacts();
+
+            //Console.WriteLine();
+
+            //RemovePhoneNumberFromContact("555-1212");
+            //GetAllContacts();
+
+            RemoveContact();
+            GetAllContacts();
+
+
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        private static void RemoveContact()
+        {
+            var contacts = db.ReadAllRecords(textFile);
+            contacts.RemoveAt(0);
+            db.WriteAllRecords(contacts, textFile);
+
+        }
+        private static void RemovePhoneNumberFromContact(string phoneNumber)
+        {
+
+            var contact = db.ReadAllRecords(textFile);
+            contact[0].PhoneNumbers.Remove(phoneNumber);
+            db.WriteAllRecords(contact, textFile);
+
+
+        }
+        private static void UpdatecContactFirstName(string firstName)
+        {
+           var contact = db.ReadAllRecords(textFile);
+           contact[0].FirstName = firstName;
+            db.WriteAllRecords(contact, textFile);
+
+           
+        }
+
+        private static void GetAllContacts()
+        {
+            var contacts = db.ReadAllRecords(textFile);
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"{contact.FirstName}, {contact.LastName}");
+            }
+        }
+        private static void CreateContact(ContactModel contact)
+        {
+            var contacts = db.ReadAllRecords(textFile);
+            contacts.Add(contact); 
+            db.WriteAllRecords(contacts, textFile);
+
+        }
+       
+
+        private static void InitializeConfiguration()
+        {
+
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json");
+
+            _config = builder.Build();
+
+        }
+    }
+}
+
+
