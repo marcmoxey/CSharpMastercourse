c7b8b05292c7ac45f9d251ef4c27dd98d5eb327f
Marc <moxiem@nychhc.org>
2025-08-21 13:42:28 -0400

    Add GetAll Contact api call

--- 36_Week/NoSqlDBSolution/SampleAPI/Controllers/ContactsController.cs
+++ 36_Week/NoSqlDBSolution/SampleAPI/Controllers/ContactsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SampleAPI.Controllers
@@ -7,5 +9,21 @@ namespace SampleAPI.Controllers
     [ApiController]
     public class ContactsController : ControllerBase
     {
+        private  MongoDBDataAccess db;
+        private readonly IConfiguration _config;
+        private  readonly string tableName = "Contacts";
+
+        public ContactsController(IConfiguration config)
+        {
+           
+            _config = config;
+            db = new MongoDBDataAccess("MongoContactsDB", _config.GetConnectionString("Default"));
+        }
+
+        [HttpGet]
+        public List<ContactModel> GetAll()
+        {
+            return db.LoadRecords<ContactModel>(tableName);
+        }
     }
 }
