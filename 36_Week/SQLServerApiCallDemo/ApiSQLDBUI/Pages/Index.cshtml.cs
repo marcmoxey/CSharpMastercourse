c29053735636cf036877c5f42cf620fd88faab24
Marc <moxiem@nychhc.org>
2025-08-22 11:32:50 -0400

    Add sql api call

---
+++ 36_Week/SQLServerApiCallDemo/ApiSQLDBUI/Pages/Index.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using ApiSQLDBUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
+using System.Text.Json;
+using System.Net.Http;
+
+namespace ApiSQLDBUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task OnGet()
+        {
+
+            await CreatePerson();
+           // await GetAllPeople();
+        }
+
+        private async Task CreatePerson()
+        {
+            PersonModel person = new PersonModel
+            {
+                Id = 7,
+                FirstName = "Marc",
+                LastName = "Moxey"
+            };
+
+            var _client = _httpClientFactory.CreateClient(); // create browser
+            var response = await _client.PostAsync("https://localhost:7261/api/People",
+               new StringContent(JsonSerializer.Serialize(person), Encoding.UTF8, "application/json"));
+        }
+
+        private async Task GetAllPeople()
+        {
+            var _client = _httpClientFactory.CreateClient(); // create browser
+            var response = await _client.GetAsync("https://localhost:7261/api/People");
+
+            List<PersonModel> people;
+
+            if(response.IsSuccessStatusCode)
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+            } else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+    }
+}
---
+++ 36_Week/SQLServerApiCallDemo/SQLApi/Controllers/PeopleController.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SQLApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private SqlCrud db;
+        private IConfiguration _config;
+
+        public PeopleController(IConfiguration config)
+        {
+            _config = config;
+            db = new SqlCrud(_config.GetConnectionString("Default"));
+        }
+
+        [HttpGet]
+        public List<PersonModel> GetAll()
+        {
+            return db.GetAllPeople();
+        }
+
+        [HttpPost]
+        public void CreatePerson(PersonModel person)
+        {
+            db.CreatePerson(person);
+        }
+    }
+}
