9b411c8d199ea3f1cccb2b6a755955e425078230
Marc Moxey <moxeymarc@gmail.com>
2025-08-23 12:45:03 -0400

    Add 2 People

--- 33_Week/NoSqlDBSolution/CosmosDBUI/Program.cs
+++ 33_Week/NoSqlDBSolution/CosmosDBUI/Program.cs
@@ -1,13 +1,45 @@
 ﻿using DataAccessLibrary;
 using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace CosmosDBUI
 {
     internal class Program
     {
         private static CosmosDBDataAccess db;
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var c = GetCosmosInfo();
+            db = new CosmosDBDataAccess(c.endpointurl,c.primarykey,c.databasename, c.containername);
+
+
+            ContactModel user = new ContactModel
+            {
+                FirstName = "Megan",
+                LastName = "Moxey"
+            };
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "nope@aol.com" });
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "mmoxey2019@stanner.org" });
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-1212" });
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-1234" });
+
+            ContactModel user2 = new ContactModel
+            {
+                FirstName = "Marc",
+                LastName = "Moxey"
+            };
+            user2.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "marc.moxey@my.liu.edu" });
+            user2.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "mmoxey2019@stanner.org" });
+            user2.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-1212" });
+            user2.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-9876" });
+
+
+            await CreateContact(user);
+            await CreateContact(user2);
+            await GetAllContacts();
+
+
+
             Console.WriteLine("Done!");
             Console.ReadLine();
         }
@@ -29,25 +61,35 @@ namespace CosmosDBUI
         {
          
         }
-        private static void GetAllContacts()
+        private static async Task GetAllContacts()
         {
-          
+            var contacts = await db.LoadRecordsASync<ContactModel>();
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"{contact.Id}: {contact.FirstName}, {contact.LastName}");
+            }
         }
-        private static void CreateContact(ContactModel contact)
+
+        private static async Task CreateContact(ContactModel contact)
         {
-        
+            await db.UpsertRecordAsync(contact);
+
         }
-        private static string GetConnectionString(string connectionStringName = "Default")
+
+
+        private static (string endpointurl, string primarykey, string databasename, string containername) GetCosmosInfo()
         {
-            string output = "";
+            (string endpointurl, string primarykey, string databasename, string containername) output;
 
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json");
 
             var config = builder.Build();
-
-            output = config.GetConnectionString(connectionStringName);
+            output.endpointurl = config.GetValue<string>("CosmosDB:EndpointUrl");
+            output.primarykey = config.GetValue<string>("CosmosDB:PrimaryKey");
+            output.databasename = config.GetValue<string>("CosmosDB:DatabaseName");
+            output.containername = config.GetValue<string>("CosmosDB:ContainerName");
 
             return output;
         }
--- 33_Week/NoSqlDBSolution/DataAccessLibrary/CosmosDBDataAccess.cs
+++ 33_Week/NoSqlDBSolution/DataAccessLibrary/CosmosDBDataAccess.cs
@@ -29,5 +29,57 @@ namespace DataAccessLibrary
             _database = _cosmosClient.GetDatabase(databaseName); // talk to the database
             _container = _database.GetContainer(containerName); // connect to specify container(table)
         }
+
+      
+        public async Task<List<T>> LoadRecordsASync<T>()
+        {
+            string sql = "Select * from c"; // c - alias, already talking to container
+            QueryDefinition queryDefinition = new QueryDefinition(sql); 
+            FeedIterator<T> feedIterator = _container.GetItemQueryIterator<T>(queryDefinition); // have records that match the query 
+
+            List<T> output = new List<T>();
+
+            while(feedIterator.HasMoreResults) // find more results that match 
+            {
+                FeedResponse<T> currentResultSet = await feedIterator.ReadNextAsync(); // grab result set
+
+                foreach (var item in currentResultSet)
+                {
+                    output.Add(item); // loop through result add to output
+                }
+            }
+
+            return output;
+        }
+
+        public async Task<T> LoadRecordById<T>(string id)
+        {
+            string sql = "Select * from c where c.id = @Id "; // c - alias, already talking to container
+            QueryDefinition queryDefinition = new QueryDefinition(sql).WithParameter("@Id", id);
+            FeedIterator<T> feedIterator = _container.GetItemQueryIterator<T>(queryDefinition); // have records that match the query 
+
+            
+
+            while (feedIterator.HasMoreResults) // find more results that match 
+            {
+                FeedResponse<T> currentResultSet = await feedIterator.ReadNextAsync(); // grab result set
+
+                foreach(var item in currentResultSet)
+                {
+                    return item; // return first value 
+                }
+            }
+
+            throw new Exception("Item not found");
+        }
+
+        // async need a task
+        // Task is similar to void 
+        public async Task UpsertRecordAsync<T>(T record)
+        {
+            await _container.UpsertItemAsync(record);  // wait for this code to be done before doing anything else 
+            
+        }
+
     }
 }
