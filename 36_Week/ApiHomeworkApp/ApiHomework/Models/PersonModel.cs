3a512c4cbdef2b721c3f3e085f58ce04523e9c70
Marc Moxey <moxeymarc@gmail.com>
2025-08-25 12:00:06 -0400

    Cleaning up models

--- 36_Week/ApiHomeworkApp/ApiHomework/Models/PersonModel.cs
+++ 36_Week/ApiHomeworkApp/ApiHomework/Models/PersonModel.cs
@@ -5,12 +5,7 @@
 
        
         
-            public string Message { get; set; }
-            public ResultModel Result { get; set; }
-            public string ApiVersion { get; set; }
-            public DateTime Timestamp { get; set; }
-            public SupportModel Support { get; set; }
-            public SocialModel Social { get; set; }
+         
         
 
     }
