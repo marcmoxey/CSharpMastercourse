9eb21269736d7cc0e44e59e5016df03df26935ee
Marc <moxiem@nychhc.org>
2025-08-29 15:11:21 -0400

    linq

---
+++ 37_Week/LinqAndLambdaSolution/LinqUI/Models/AddressModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqUI.Models
+{
+    public class AddressModel
+    {
+        public int Id { get; set; }
+        public int ContactId { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
--- 37_Week/LinqAndLambdaSolution/LinqUI/Program.cs
+++ 37_Week/LinqAndLambdaSolution/LinqUI/Program.cs
@@ -1,10 +1,89 @@
-﻿namespace LinqUI
+﻿using LinqUI.Models;
+
+namespace LinqUI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //LambdaTests();
+            LinqTests();
+            Console.WriteLine("Done!");
+            Console.ReadLine();
         }
+
+        private static void LambdaTests()
+        {
+            var data = SampleData.GetContactData();
+
+            //var results = data.Where(x => x.Addresses.Count() > 1); 
+
+            //foreach(var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName}, {item.LastName}");
+            //}
+
+            //var results = data.Select(x => x.FirstName);
+            //foreach(var item in results)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var results = data.Take(2);
+
+            //foreach(var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName}, {item.LastName}");
+            //}
+
+            //var results = data.Skip(2).Take(2);
+            //foreach(var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName}, {item.LastName}");
+            //}
+
+            //var results = data.OrderByDescending(x => x.LastName);
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName}, {item.LastName}");
+            //}
+        }
+
+        private static void LinqTests()
+        {
+            var contacts = SampleData.GetContactData();
+            var addresses = SampleData.GetAddressData();
+
+            //var results = (from c in contacts 
+            //               join a in addresses
+            //               on c.Id equals a.ContactId
+            //               select new {c.FirstName, c.LastName, a.City, a.State}); // new aynomous object
+
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName}, {item.LastName} from {item.City}, {item.State}");
+            ////}
+
+            //var results = (from c in contacts
+            //               select new { c.FirstName, c.LastName, Addresses = addresses.Where(x => x.ContactId == c.Id) });
+
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName}, {item.LastName} - {item.Addresses.Count()}");
+            //}
+
+            //}
+
+            var results = (from c in contacts
+                           select new { c.FirstName, c.LastName, Addresses = addresses.Where(a => c.Addresses.Contains(a.Id)) });
+
+            foreach (var item in results)
+            {
+                Console.WriteLine($"{item.FirstName}, {item.LastName} - {item.Addresses.Count()}");
+            }
+
+        }
+
+
     }
 }
