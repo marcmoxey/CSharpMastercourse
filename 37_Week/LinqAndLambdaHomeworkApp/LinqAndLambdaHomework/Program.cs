9cfda2229a4d121cad3aad9e75fb27d452361388
Marc Moxey <moxeymarc@gmail.com>
2025-08-30 10:02:14 -0400

    Add homework for linq and lambdas

---
+++ 37_Week/LinqAndLambdaHomeworkApp/LinqAndLambdaHomework/Program.cs
@@ -0,0 +1,45 @@
+﻿using LinqAndLambdaHomework;
+
+
+//LambdaTest();
+LinqTest();
+Console.ReadLine();
+
+void LambdaTest()
+{
+    var data = SampleData.GetPersonData();
+
+    //var results = data.OrderByDescending(x => x.FirstName);
+
+    //foreach (var item in results)
+    //{
+    //    Console.WriteLine($"{item.FirstName}, {item.LastName}");
+    //}
+
+    var results = data.Skip(1).Take(2);
+    foreach (var item in results)
+    {
+        Console.WriteLine($"{item.FirstName}, {item.LastName}");
+    }
+}
+void LinqTest()
+{
+    var people = SampleData.GetPersonData();
+
+    //var results = (from p in people
+    //               select new { p.FirstName, p.LastName });
+
+    //foreach (var item in results)
+    //{
+    //    Console.WriteLine($"{item.FirstName}, {item.LastName}");
+    //}
+
+    var results = (from p in people
+                   where p.Id == 1
+                   select new {p.FirstName, p.LastName});
+
+    foreach (var item in results)
+    {
+        Console.WriteLine($"{item.FirstName}, {item.LastName}");
+    }
+}
---
+++ 37_Week/LinqAndLambdaHomeworkApp/LinqAndLambdaHomework/SampleData.cs
@@ -0,0 +1,27 @@
+﻿using LinqAndLambdaHomework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLambdaHomework
+{
+    public static class SampleData
+    {
+        public static List<PersonModel> GetPersonData()
+        {
+                List<PersonModel> output = new List<PersonModel>
+                {
+                    new PersonModel { Id = 1, FirstName = "Marc", LastName = "Moxey"},
+                    new PersonModel { Id = 2, FirstName = "Megan", LastName = "Moxey"},
+                    new PersonModel { Id = 3, FirstName = "Kenneth", LastName = "Moxey"}
+                };
+            return output;
+        }
+     
+
+      
+    }
+}
