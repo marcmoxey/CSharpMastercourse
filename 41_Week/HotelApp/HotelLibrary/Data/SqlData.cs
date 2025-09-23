70ed46509f8dd5bca8014aefcd2c9bd2cbc50667
Marc <moxiem@nychhc.org>
2025-09-23 15:35:52 -0400

    Add Book Guest

---
+++ 41_Week/HotelApp/HotelLibrary/Data/SqlData.cs
@@ -0,0 +1,47 @@
+﻿using HotelLibrary.Data;
+using HotelLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLibrary.Data
+{
+    public  class SqlData
+    {
+        private readonly ISQLDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+        public SqlData(ISQLDataAccess db)
+        {
+            _db = db;
+           
+        }
+        public List<RoomTypeModel> GetAvailableRooms(DateTime startDate, DateTime endDate)
+        {
+            string sql = "dbo.spRoomtypes_GetAvailableTypes";
+            var output = _db.LoadData<RoomTypeModel, dynamic>(sql, new { StartDate = startDate, EndDate = endDate },connectionStringName, true);
+            return output; 
+          
+        }
+
+        public void BookGuest(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId)
+        {
+            // get guest that wants to book 
+            GuestModel guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuests_Insert", new { FirstName = firstName, LastName = lastName}, connectionStringName, true).First();
+
+
+            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypes where Id = @Id", new { Id = roomTypeId}, connectionStringName, false).First();
+
+            TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date); // number of days(timespan)
+
+
+            // get list of available room
+            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailable", new {startDate, endDate, roomTypeId }, connectionStringName, true);
+
+            // book the room
+            _db.SaveData("dbo.spBookings_Insert", new { RoomId = availableRooms.First().Id, GuestId = guest.Id, StartDate = startDate, EndDate = endDate, TotalCost = timeStaying.Days * roomType.Price }, connectionStringName, true);
+          
+        }
+    }
+}
