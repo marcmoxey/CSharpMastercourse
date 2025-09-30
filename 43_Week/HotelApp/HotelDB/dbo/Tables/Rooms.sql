09bc416129d775ed301bfa94f1051d62197093f7
Marc <moxiem@nychhc.org>
2025-09-30 15:29:26 -0400

    Add Week 43

---
+++ 43_Week/HotelApp/HotelDB/dbo/Tables/Rooms.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Rooms]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [RoomNumber] VARCHAR(10) NOT NULL, 
+    [RoomTypeId] INT NOT NULL, 
+    CONSTRAINT [FK_Rooms_RoomTypes] FOREIGN KEY ([RoomTypeId]) REFERENCES RoomTypes(Id)
+)
