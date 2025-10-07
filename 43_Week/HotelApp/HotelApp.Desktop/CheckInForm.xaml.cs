71fb2e2d93925555b19dd9102616e2e4362164fc
Marc <moxiem@nychhc.org>
2025-10-07 13:34:15 -0400

    Add WPF

---
+++ 43_Week/HotelApp/HotelApp.Desktop/CheckInForm.xaml.cs
@@ -0,0 +1,34 @@
+﻿using HotelLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for CheckInForm.xaml
+    /// </summary>
+    public partial class CheckInForm : Window
+    {
+        private BookingFullModel _data = null;
+        public CheckInForm()
+        {
+            InitializeComponent();
+        }
+
+        public void PopulateCheckInInfo(BookingFullModel data)
+        {
+            _data = data;
+        }
+    }
+}
---
+++ 43_Week/HotelApp/HotelApp.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using HotelAppLibrary.Data;
+using HotelLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IDatabaseData _db;
+        public MainWindow(IDatabaseData db)
+        {
+            InitializeComponent();
+            _db = db;
+        }
+
+        private void searchForGuest_Click(object sender, RoutedEventArgs e)
+        {
+
+            List<BookingFullModel> bookings = _db.SearchBookings(lastNameText.Text);
+            resultsList.ItemsSource = bookings;
+            
+        }
+
+        private void CheckInButton_Click(object sender, RoutedEventArgs e)
+        {
+            var checkInForm = App.serviceProvider.GetService<CheckInForm>(); // open check in form
+            var model = (BookingFullModel)((Button)e.Source).DataContext; // source a button - then cast as a bookingfullModel; must be that type
+            checkInForm.PopulateCheckInInfo(model); // forms has info it needs
+            checkInForm.Show();
+        }
+
+    }
+}
